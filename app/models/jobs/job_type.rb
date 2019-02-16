9096fe4d6c546b511743b601eee585c7a8ff4ddd
Tim Mckenzie <tim@mobilecause.com>
2019-02-15 22:50:28 -0700

    wip

--- app/models/jobs/job_type.rb
+++ app/models/jobs/job_type.rb
@@ -18,4 +18,18 @@ class Jobs::JobType < ActiveHash::Base
       data.detect { |d| d[:name] == name }
     end
   end
+
+  def self.make_statusable
+    self.data.each do |member|
+      define_singleton_method(member[:name].downcase) do
+        find_by_name(member[:name])
+      end
+    end
+
+    self.data.each do |member|
+      define_method("#{member[:name].downcase}?") do
+        member[:id] == self.id
+      end
+    end
+  end
 end
