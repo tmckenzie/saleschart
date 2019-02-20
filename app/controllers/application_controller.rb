ad8a29d60ef25e94c544cb257fffed0ced78ef25
Tim Mckenzie <tim@mobilecause.com>
2019-02-20 13:04:08 -0700

    wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery
+  protect_from_forgery prepend: true
   before_action :authenticate_user!
   include Mixins::Navigation
 
